43f0475e6cbafede0ded5fe6e0c885999bd32c10
didala <javieraladid@gmail.com>
2025-07-16 21:27:43 +0200

    Tests working, added some more tests

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -9,8 +9,12 @@ module.exports = {
 	roots: ['<rootDir>/src'],
 	transform: {
 	  '^.+\\.(ts|tsx)$': 'ts-jest',
+	  '^.+\\.svg$': 'jest-transform-stub',
 	},
 	moduleNameMapper: {
 	  '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+      '^@/(.*)$': '<rootDir>/src/$1',
+      '^@components/(.*)$': '<rootDir>/src/components/$1',
+	  '\\.svg$': '<rootDir>/src/__mocks__/svgMock.js',
 	},
   };
---
+++ frontend/src/__mocks__/svgMock.js
@@ -0,0 +1,3 @@
+const stub = 'test-file-stub';
+module.exports = stub;
+module.exports.default = stub; 
